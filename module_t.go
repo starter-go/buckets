9943660019b830ea0d5e7dd78272bf0a1c348c3c
kge <kge@bitwormhole.com>
2026-06-27 10:57:54 +0800

    update to v0.0.7

--- module_t.go
+++ module_t.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/buckets"
-	theModuleVersion  = "v0.0.6"
-	theModuleRevision = 6
+	theModuleVersion  = "v0.0.7"
+	theModuleRevision = 7
 )
 
 ////////////////////////////////////////////////////////////////////////////////
